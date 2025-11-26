66fa1bc0ec2b8f3ba88337288e983b85859374c0
prathamesh.k0710 <prathamesh.k0710@gmail.com>
2025-11-27 01:11:06 +0530

    removed unwanted workflow

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,9 @@ const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 
+console.log("SERVICE_ID: ", SERVICE_ID)
+console.log("TEMPLATE_ID: ", TEMPLATE_ID)
+console.log("PUBLIC_KEY: ", PUBLIC_KEY)
 
 const Contact = () => {
   const { ref, isInView } = useScrollAnimation()
@@ -19,6 +22,9 @@ const Contact = () => {
   })
 
   const handleSubmit = (e) => {
+    console.log("SERVICE_ID1: ", SERVICE_ID)
+    console.log("TEMPLATE_ID1: ", TEMPLATE_ID)
+    console.log("PUBLIC_KEY1: ", PUBLIC_KEY)
     e.preventDefault()
       emailjs.send(SERVICE_ID, TEMPLATE_ID,{
         from_name: formData.name,
