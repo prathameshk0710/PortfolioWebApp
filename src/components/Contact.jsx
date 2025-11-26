82b3d7c44c89c22b9a198457a96e2d2a42835126
prathamesh.k0710 <prathamesh.k0710@gmail.com>
2025-11-27 00:54:39 +0530

    removed logs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,9 +9,6 @@ const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 
-console.log("SERVICE_ID: ", SERVICE_ID)
-console.log("TEMPLATE_ID: ", TEMPLATE_ID)
-console.log("PUBLIC_KEY: ", PUBLIC_KEY)
 
 const Contact = () => {
   const { ref, isInView } = useScrollAnimation()
@@ -22,9 +19,6 @@ const Contact = () => {
   })
 
   const handleSubmit = (e) => {
-    console.log("SERVICE_ID1: ", SERVICE_ID)
-    console.log("TEMPLATE_ID1: ", TEMPLATE_ID)
-    console.log("PUBLIC_KEY1: ", PUBLIC_KEY)
     e.preventDefault()
       emailjs.send(SERVICE_ID, TEMPLATE_ID,{
         from_name: formData.name,
